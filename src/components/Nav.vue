1dd92d71e7359c45a38db0677c6bfe347c77ab7f
Chris Wu <zezemint@gmail.com>
2023-06-14 16:39:36 -0700

    impl/ init nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="nav">
+    <ul class="nav-list">
+      <li class="nav-item">
+        <a href="#" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">About</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">Services</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+.nav {
+  margin-top: 2rem;
+}
+
+.nav-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.nav-item {
+  margin-bottom: 1rem;
+}
+
+.nav-link {
+  display: block;
+  padding: 0.5rem;
+  color: #ffffff;
+  text-decoration: none;
+  transition: background-color 0.3s;
+}
+
+.nav-link:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 0.25rem;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,16 +2,33 @@
     <div class="w-screen h-screen flex">
       <div class="sidebar bg-sky-900 text-zinc-100">
         <!-- Content of the sidebar -->
+        <h1 class="text-2xl font-bold">Dashboard</h1>
+        <hr>
+        <Nav />
       </div>
       <div class="content flex-1">
         <!-- Main content -->
+        <p class="mt-4 italic">
+          "Success is not final, failure is not fatal: It is the courage to continue that counts."
+          - Winston Churchill
+        </p>
       </div>
     </div>
   </template>
   
+  <script>
+  import Nav from '@/components/Nav.vue';
+  
+  export default {
+    components: {
+      Nav,
+    },
+  };
+  </script>
+  
   <style>
   .sidebar {
-    width: 12cm;
+    width: 8cm;
     padding: 2rem;
   }
   
